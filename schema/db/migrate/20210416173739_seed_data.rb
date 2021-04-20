98cac1ef988edb3740a91a525b12d41c113d4210
kari <mr.kari.ikonen@gmail.com>
2021-04-20 23:03:36 +0300

    ADD create system user

--- schema/db/migrate/20210416173739_seed_data.rb
+++ schema/db/migrate/20210416173739_seed_data.rb
@@ -11,9 +11,27 @@ class SeedData < ActiveRecord::Migration[6.1]
   ]
 
   def up
+    create_roles
+    create_system_user
+  end
+
+  def create_roles
     ROLES.each do |role|
       sql = ActiveRecord::Base.sanitize_sql(["insert into roles(name, admin) values (:name, :admin)", name: role[:name], admin: role[:admin]])
-      execute sql
+      execute(sql)
     end
+  end
+
+  def create_system_user
+    rs = execute("select id from roles where admin")
+
+    user = {
+      name: 'System',
+      email: 'system@system',
+      role_id: rs.first["id"]
+    }
+
+    sql = ActiveRecord::Base.sanitize_sql(["insert into users(name, email, role_id) values (:name, :email, :role_id)", name: user[:name], email: user[:email], role_id: user[:role_id]])
+    execute(sql)
   end
 end
