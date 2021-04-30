9ff59f19bf80dc781ce3632b08c291cf4c8cf725
kari <mr.kari.ikonen@gmail.com>
2021-04-30 10:57:30 +0300

    ADD join/create directly in UI

--- ui-service/src/components/ChannelsComponent.tsx
+++ ui-service/src/components/ChannelsComponent.tsx
@@ -18,10 +18,32 @@ export class ChannelsComponent extends React.Component<Props> {
     autobind(this);
   }
 
+  onJoinChannel(e: any) {
+    Emitter.emit('channel.join.show');
+  }
+
+  onCreateChannel(e: any) {
+    Emitter.emit('channel.create.show');
+  }
+
   render() {
     return (
       <div className="m-1 border border-dark sl-channels-container">
         <div className="container-fluid sl-channels-content">
+          <div className="row no-gutters">
+            <div className="col-auto">
+              <button className="btn btn-primary btn-sm mt-1 mb-1"
+                onClick={this.onJoinChannel}>
+                Join
+              </button>
+             </div>
+            <div className="col-auto">
+              <button className="btn btn-success btn-sm ml-1 mt-1 mb-1"
+                onClick={this.onCreateChannel}>
+                Add
+              </button>
+             </div>
+          </div>
           <div className="row no-gutters">
             {this.props.userInfo.channels.map((channel: any) => (
               <div key={channel.id} className="col-12">
