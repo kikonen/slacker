4e19dac44ed5dba9f7d6b9c3642e9193a5454395
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-02-02 21:50:10 +0200

    FIX build

--- scripts/build_production.sh
+++ scripts/build_production.sh
@@ -2,4 +2,4 @@ DIR=`dirname $0`
 DIR=`realpath $DIR`
 ROOT_DIR=`dirname $DIR`
 
-time docker-compose --project-name slacker -f docker-compose.yml -f docker-compose.build.yml build "$@"
+time docker-compose --project-name slacker_build -f docker-compose.yml -f docker-compose.build.yml build "$@"
