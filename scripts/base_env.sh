2130db32c51a0c13413cde55ff4304658917bb25
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-02-04 21:08:50 +0200

    FIX refactor

--- scripts/base_env.sh
+++ scripts/base_env.sh
@@ -2,12 +2,13 @@
 
 DIR=`realpath \`dirname $0\``
 ROOT_DIR=`dirname $DIR`
+DOCKER_PRODUCTION_NAME=slacker_production
 DOCKER_BUILD_NAME=slacker_build
 
 echo "DOCKER_ENV=${DOCKER_ENV}"
 
 if [[ $DOCKER_ENV == 'production' ]]; then
-    DOCKER_COMPOSE="docker-compose --project-dir=$ROOT_DIR -f docker-compose.yml -f docker-compose.production.yml"
+    DOCKER_COMPOSE="docker-compose --project-dir=$ROOT_DIR --project-name ${DOCKER_PRODUCTION_NAME} -f docker-compose.yml -f docker-compose.production.yml"
 elif [[ $DOCKER_ENV == 'build' ]]; then
     DOCKER_COMPOSE="docker-compose --project-dir=${ROOT_DIR} --project-name ${DOCKER_BUILD_NAME} -f docker-compose.yml -f docker-compose.build.yml"
 else
